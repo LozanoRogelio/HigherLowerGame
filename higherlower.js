8bb9b791885fc3da747e9ff6b4bcea6df9587beb
Rogelio Lozano <lozano.rogelio92@gmail.com>
2023-02-06 11:51:16 -0600

    Finished part 1 of the Enhanced Higher-Lower Game

--- higherlower.js
+++ higherlower.js
@@ -1,5 +1,22 @@
-let num = Math.floor(Math.random() * 20) + 1;
+while (true) {
+   let askMaxNum = prompt("What will be the max number in the game?")
+   if (askMaxNum < 1 || isNaN(askMaxNum)) {
+      alert("Invalid Entry!")
+      continue;
+   }
+   if (askMaxNum % 1 != 0) {
+      askMaxNum = Math.round(askMaxNum)
+   }
+
+   let maxNum = document.getElementById("maxNum")
+   maxNum.innerHTML = "Guess a number between 1 and " + askMaxNum;
+   break;
+}
+
+
+let num = Math.floor(Math.random() * askMaxNum) + 1;
 console.log(num)
+
 
 function doGuess() {
    let guess = Number(document.getElementById("guess").value);
@@ -15,6 +32,4 @@ function doGuess() {
    else {
       message.innerHTML = "No, try a higher number"
    }
-
-
 }
